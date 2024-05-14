ec0f4547b3d661913c4b33424fc701205e0aae7d
lzl-hello <2309437043@qq.com>
2024-05-14 19:45:53 +0800

    user_one for dev

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def add_numbers(a, b):
     """
     return a + b
 
-def add_numbers(a, b):
+def jian_numbers(a, b):
     """
     计算两个数之差的函数
 
@@ -25,6 +25,18 @@ def add_numbers(a, b):
     """
     return a - b
 
+def cheng_numbers(a, b):
+    """
+    计算两个数之差的函数
+
+    参数:
+    a (int or float): 第一个数
+    b (int or float): 第二个数
+
+    返回:
+    int or float: 两个数的乘积
+    """
+    return a * b
 
 print("hello user_one")
 print("git push origin main")
